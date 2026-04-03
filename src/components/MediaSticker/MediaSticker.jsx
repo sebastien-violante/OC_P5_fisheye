f70c2c0b6980b127f72aa8562d8183821e1ee6fb
sebastien-violante <sebastien.violante@gmail.com>
2026-04-03 22:28:08 +0200

    affichage videos

--- src/components/MediaSticker/MediaSticker.jsx
+++ src/components/MediaSticker/MediaSticker.jsx
@@ -2,11 +2,19 @@ import styles from './MediaSticker.module.css'
 import Image from 'next/image'
 
 export default function MediaSticker({medium, openLightBox, increaseLikes, likes}) {
+    console.log(medium)
     return (
         <figure className={styles.mediaContainer}>
             <button className={styles.btnImage} onClick={() => openLightBox(medium)}>
+                { !medium.video &&
                 <Image height={300} width={350} className={styles.image} src={`/pictures/${medium.image}`} alt={medium.title}/>
-            </button>
+                }
+                { medium.video && 
+                <video controls width="100%">
+                    <source src={`/pictures/${medium.video}`} controls  style={{ width: "100%" }} type="video/mp4" />
+                </video>
+                }
+                </button>
             <div className={styles.caption}>
                 <p className={styles.title}>{medium.title}</p>
                 <div className={styles.likes}>
